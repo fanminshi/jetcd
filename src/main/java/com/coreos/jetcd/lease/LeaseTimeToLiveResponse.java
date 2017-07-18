eab8368f4cfcbc464b58885af6f6d09b1adb9c9b
fanmin shi <fanmin.shi@coreos.com>
2017-07-18 15:48:04 -0700

    lease: let lease responses extend AbstractResponse

--- src/main/java/com/coreos/jetcd/lease/LeaseTimeToLiveResponse.java
+++ src/main/java/com/coreos/jetcd/lease/LeaseTimeToLiveResponse.java
@@ -1,35 +1,26 @@
 package com.coreos.jetcd.lease;
 
+import com.coreos.jetcd.data.AbstractResponse;
 import com.coreos.jetcd.data.ByteSequence;
-import com.coreos.jetcd.data.Header;
 import java.util.List;
+import java.util.concurrent.atomic.AtomicReference;
+import java.util.stream.Collectors;
 
-public class LeaseTimeToLiveResponse {
+public class LeaseTimeToLiveResponse extends
+    AbstractResponse<com.coreos.jetcd.api.LeaseTimeToLiveResponse> {
 
-  private Header header;
-  private long leaseId;
-  private long ttl;
-  private long grantedTtl;
-  private List<ByteSequence> keys;
+  private final AtomicReference<List<ByteSequence>> keysRef;
 
-  public LeaseTimeToLiveResponse(Header header, long leaseId, long ttl, long grantedTtl,
-      List<ByteSequence> keys) {
-    this.header = header;
-    this.leaseId = leaseId;
-    this.ttl = ttl;
-    this.grantedTtl = grantedTtl;
-    this.keys = keys;
-  }
-
-  public Header getHeader() {
-    return header;
+  public LeaseTimeToLiveResponse(com.coreos.jetcd.api.LeaseTimeToLiveResponse response) {
+    super(response, response.getHeader());
+    keysRef = new AtomicReference<>();
   }
 
   /**
    * ID is the lease ID from the keep alive request.
    */
   public long getID() {
-    return leaseId;
+    return getResponse().getID();
   }
 
   /**
@@ -37,21 +28,34 @@ public class LeaseTimeToLiveResponse {
    * the lease will expire in under TTL+1 seconds.
    */
   public long getTTl() {
-    return ttl;
+    return getResponse().getTTL();
   }
 
   /**
    * GrantedTTL is the initial granted time in seconds upon lease creation/renewal.
    */
   public long getGrantedTTL() {
-    return grantedTtl;
+    return getResponse().getGrantedTTL();
   }
 
   /**
    * Keys is the list of keys attached to this lease.
    */
   public List<ByteSequence> getKeys() {
+    List<ByteSequence> keys = keysRef.get();
+    if (keys == null) {
+      synchronized (keysRef) {
+        keys = keysRef.get();
+        if (keys == null) {
+          keys = getResponse().getKeysList().stream()
+              .map(byteStrings -> ByteSequence.fromBytes(byteStrings.toByteArray()))
+              .collect(Collectors.toList());
+
+          keysRef.set(keys);
+        }
+      }
+    }
+
     return keys;
   }
-
 }

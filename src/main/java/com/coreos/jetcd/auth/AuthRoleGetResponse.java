dcf51261a1127caf090ebb69655bd93ea1d08705
fanmin shi <fanmin.shi@coreos.com>
2017-07-18 15:44:35 -0700

    auth: refactor auth responses to extend AbstractResponse

--- src/main/java/com/coreos/jetcd/auth/AuthRoleGetResponse.java
+++ src/main/java/com/coreos/jetcd/auth/AuthRoleGetResponse.java
@@ -1,29 +1,62 @@
 package com.coreos.jetcd.auth;
 
 import com.coreos.jetcd.Auth;
+import com.coreos.jetcd.auth.Permission.Type;
+import com.coreos.jetcd.data.AbstractResponse;
 import com.coreos.jetcd.data.ByteSequence;
-import com.coreos.jetcd.data.Header;
 import java.util.List;
+import java.util.concurrent.atomic.AtomicReference;
+import java.util.stream.Collectors;
 
 /**
  * AuthRoleGetResponse returned by {@link Auth#roleGet(ByteSequence)} contains
  * a header and a list of permissions.
  */
-public class AuthRoleGetResponse {
+public class AuthRoleGetResponse extends
+    AbstractResponse<com.coreos.jetcd.api.AuthRoleGetResponse> {
 
-  private final Header header;
-  private final List<Permission> permissions;
+  private final AtomicReference<List<Permission>> permissionsRef;
 
-  public AuthRoleGetResponse(Header header, List<Permission> permissions) {
-    this.header = header;
-    this.permissions = permissions;
+  public AuthRoleGetResponse(com.coreos.jetcd.api.AuthRoleGetResponse response) {
+    super(response, response.getHeader());
+    permissionsRef = new AtomicReference<>();
   }
 
-  public Header getHeader() {
-    return header;
+  private static Permission toPermission(com.coreos.jetcd.api.Permission perm) {
+    ByteSequence key = ByteSequence.fromBytes(perm.getKey().toByteArray());
+    ByteSequence rangeEnd = ByteSequence.fromBytes(perm.getRangeEnd().toByteArray());
+    Permission.Type type;
+    switch (perm.getPermType()) {
+      case READ:
+        type = Type.READ;
+        break;
+      case WRITE:
+        type = Type.WRITE;
+        break;
+      case READWRITE:
+        type = Type.READWRITE;
+        break;
+      default:
+        type = Type.UNRECOGNIZED;
+    }
+    return new Permission(type, key, rangeEnd);
   }
 
   public List<Permission> getPermissions() {
+    List<Permission> permissions = permissionsRef.get();
+    if (permissions == null) {
+      synchronized (permissionsRef) {
+        permissions = permissionsRef.get();
+        if (permissions == null) {
+          permissions = getResponse().getPermList().stream()
+              .map(AuthRoleGetResponse::toPermission)
+              .collect(Collectors.toList());
+        }
+
+        permissionsRef.lazySet(permissions);
+      }
+    }
+
     return permissions;
   }
 }
